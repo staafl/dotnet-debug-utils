c1b55e333318d66f22461841dd6c3ed1c743833e
Velko Nikolov <velko.nikolov@tick42.com>
2014-02-03 19:41:21 +0200

    strinq

--- src/Strinq.cs
+++ src/Strinq.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class Strinq
 {
@@ -9,10 +10,26 @@ public static class Strinq
         var prop = typeof(TSource).GetProperty(property);
         return seq.Select(s => prop.GetValue(s));
     }
+    
+    public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> seq, object value)
+    {
+        return seq.Where(s => Object.Equals(value, s));
+    }
+    
+    public static IEnumerable<TSource> WhereRegex<TSource>(this IEnumerable<TSource> seq, string regex)
+    {
+        return seq.Where(s => Regex.IsMatch(s + "", regex));
+    }
    
     public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> seq, string property, object value)
     {
         var prop = typeof(TSource).GetProperty(property);
         return seq.Where(s => Object.Equals(value, prop.GetValue(s)));
+    }
+    
+    public static IEnumerable<TSource> WhereRegex<TSource>(this IEnumerable<TSource> seq, string property, string regex)
+    {
+        var prop = typeof(TSource).GetProperty(property);
+        return seq.Where(s => Regex.IsMatch(prop.GetValue(s) + "", regex));
     }
 }
