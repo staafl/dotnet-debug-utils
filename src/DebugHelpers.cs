07419f5571815535224508b946072062c353a698
Velko Nikolov <velko.nikolov@tick42.com>
2014-12-01 15:48:49 +0200

    Eval

--- src/DebugHelpers.cs
+++ src/DebugHelpers.cs
@@ -8,6 +8,41 @@ using System.Threading;
 
 public static class DebugHelpers
 {
+    public static object Eval(this object obj, string path)
+    {
+      string soFar = "<object>";
+      const BindingFlags FLAGS = 
+          BindingFlags.Instance |
+          BindingFlags.NonPublic | 
+          BindingFlags.Public | 
+          BindingFlags.IgnoreCase |
+          BindingFlags.FlattenHierarchy;
+         
+      foreach (string elem in path.Split('.'))
+      {
+          if (obj == null)
+          {
+            return soFar + " = <null>";
+          }
+          soFar += "." + elem;
+          FieldInfo field = obj.GetType().GetField(elem, FLAGS);
+          if (field != null)
+          {
+            obj = field.GetValue(obj);
+            continue;
+          }
+          PropertyInfo prop = obj.GetType().GetProperty(elem, FLAGS);
+          if (prop != null)
+          {
+            obj = prop.GetValue(obj);
+            continue;
+          }
+          
+          return soFar + " not found";
+      }
+      return obj ?? (object)"<null>";
+    }
+    
     public static void BreakOnFileAccess(string path) 
     {
         var msg = Path.GetFullPath(path);
